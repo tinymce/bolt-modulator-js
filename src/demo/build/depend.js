1779fca65e83a1fd134ce533b1aa03a653743292
Marli Oshlack <marli.oshlack@ephox.com>
2012-08-29 21:07:52 +1000

    Specify version of wrap-jquery

--- src/demo/build/depend.js
+++ src/demo/build/depend.js
@@ -7,7 +7,7 @@ var dependencies = [
   {
     name: "wrap-jquery",
     repository: "buildrepo2",
-    version: "latest",
+    version: '1.4.2/1.4.2.0',
     source: "wrap-jquery.zip",
     targets: [
       { name: "compile/ephox*.js", path: lib + "/run/depend" }
